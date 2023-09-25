f3d06b8adb6bbd566fd550f76e00f76b38e4416e
brigitabiederman <brigita@modularsystems.io>
2023-09-25 10:30:25 -0500

    added starting files and structuring along with a navbar and body

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+import Intro from './components/Intro/Intro';
+
+
+function App() {
+  return (
+    <div className="App">  
+    <Navbar /> 
+    <Intro />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Intro/Intro.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./intro.css";
+
+const intro = () => {
+  return (
+    <section className="intro">
+      <div className="introContent">
+        <span className="intro </section>Text">, my name is</span>
+      </div>
+    </section>
+  );
+};
+export default intro;
